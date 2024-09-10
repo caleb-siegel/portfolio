8e2e1881475028fd5921957876cae90b0e3a3ace
Caleb Siegel <caleb.siegel@gmail.com>
2024-09-10 11:41:27 -0400

    integrate with emailJS to send email when submitting form

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 <!-- To run slider functionality -->
 run "npm install swiper"
+
+<!-- To run emailJS -->
+run "npm install emailjs-com"
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,41 +1,75 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import emailjs from 'emailjs-com';
 import "./contact.css";
 
 const Contact = () => {
+  const [submitted, setSubmitted] = useState(false)
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_w3zo357', 'template_mda9ref', form.current, 'UHOqcMeIWNczKNA_1')
+      .then((result) => {
+        console.log(result.text);
+        setSubmitted(true);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
   return (
     <section className="contact container section" id='contact'>
       <h2 className="section__title">Reach Out</h2>
-
       <div className="contact__container grid">
         <div className="contact__info">
           <h3 className="contact__title">Let's Talk</h3>
           <p className="contact__details">Shoot me an email!</p>
         </div>
-
-        <form action="" className="contact__form">
-          <div className="contact__form-group">
-            <div className="contact__form-div">
-              <input type="text" className="contact__form-input" placeholder='Insert your name'/>
+        {!submitted ?
+          <form ref={form} onSubmit={sendEmail} className="contact__form">
+            <div className="contact__form-group">
+              <div className="contact__form-div">
+                <input
+                  type="text"
+                  className="contact__form-input"
+                  placeholder='Insert your name'
+                  name="user_name"
+                />
+              </div>
+              <div className="contact__form-div">
+                <input
+                  type="email"
+                  className="contact__form-input"
+                  placeholder='Insert your email'
+                  name="user_email"
+                />
+              </div>
             </div>
-
             <div className="contact__form-div">
-              <input type="email" className="contact__form-input" placeholder='Insert your email'/>
+              <input
+                type="text"
+                className="contact__form-input"
+                placeholder='Insert your subject'
+                name="subject"
+              />
             </div>
-          </div>
-
-          <div className="contact__form-div">
-            <input type="text" className="contact__form-input" placeholder='Insert your subject'/>
-          </div>
-
-          <div className="contact__form-div contact__form-area">
-            <textarea name="" id="" cols="30" rows="10" className='contact__form-input' placeholder='Write your message'></textarea>
-          </div>
-
-          <button className="btn">Send Message</button>
-        </form>
+            <div className="contact__form-div contact__form-area">
+              <textarea
+                name="message"
+                cols="30"
+                rows="10"
+                className='contact__form-input'
+                placeholder='Write your message'
+              />
+            </div>
+            <button type="submit" className="btn">Send Message</button>
+          </form>
+        :
+        <p className="contact__details">Your email has been sent. I will try to respond shortly.</p>
+        }
       </div>
     </section>
   )
 }
 
-export default Contact
+export default Contact;
